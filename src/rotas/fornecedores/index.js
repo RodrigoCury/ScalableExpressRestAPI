42eb6b5e2063dd9190a8a3ab45fc423c0648c362
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-19 11:09:23 -0300

    Suplier Routes

---
+++ src/rotas/fornecedores/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+const TabelaFornecedor = require('./TabelaFornecedor')
+
+router.get('/', async (req, res) => {
+    const resultados = await TabelaFornecedor.listar()
+    res.send(
+        JSON.stringify(resultados)
+    )
+})
+
+router.post('/', (req, res) => {
+
+})
+
+module.exports = router
