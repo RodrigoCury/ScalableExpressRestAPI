2938ff8e33ed2fc97192e91e396a951a46d42b5a
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-19 12:10:34 -0300

    Insert method

--- src/rotas/fornecedores/TabelaFornecedor.js
+++ src/rotas/fornecedores/TabelaFornecedor.js
@@ -4,6 +4,10 @@ class Fornecedor {
     listar() {
         return Model.findAll()
     }
+
+    inserir(fornecedor) {
+        return Model.create(fornecedor)
+    }
 }
 
 module.exports = new Fornecedor();
