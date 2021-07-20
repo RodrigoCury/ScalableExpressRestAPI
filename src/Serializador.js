a06310d980a2a3e0cbcfaece87caa53f6d893c5d
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-20 12:13:43 -0300

    Serializer can serialize to XML aswell

--- src/Serializador.js
+++ src/Serializador.js
@@ -1,3 +1,6 @@
+const ConteudoNaoSuportado = require("./erros/ConteudoNaoSuportado")
+const jsontoxml = require('jsontoxml')
+
 class Serializador {
 
     constructor(contentType) {
@@ -5,15 +8,35 @@ class Serializador {
     }
 
     serializar(dados) {
+        dados = this.filtrar(dados)
+
         if (this.contentType === 'application/json') {
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
+        } else if (this.contentType === 'application/xml') {
+            return this.xml(dados)
+        } else {
+            throw new ConteudoNaoSuportado(this.contentType)
         }
     }
 
     json(dados) {
         return JSON.stringify(dados)
+    }
+
+    xml(dados) {
+        let tag = this.tagSingular
+
+        if (Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map(dado => {
+                return {
+                    [this.tagSingular]: dado
+                }
+            })
+        }
+        return jsontoxml({
+            [tag]: dados
+        })
     }
 
     filtarObjeto(dados) {
@@ -45,6 +68,8 @@ class SerializadorFornecedor extends Serializador {
     constructor(contentType, camposExtras = []) {
         super(contentType)
         this.camposPublicos = ['id', 'empresa', 'categoria', ...camposExtras]
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -52,6 +77,8 @@ class SerializadorErro extends Serializador {
     constructor(contentType, camposExtras = []) {
         super(contentType)
         this.camposPublicos = ['idErro', 'message', ...camposExtras]
+        this.tagSingular = 'Erro'
+        this.tagPlural = 'Erros'
     }
 }
 
@@ -59,5 +86,8 @@ module.exports = {
     Serializador,
     SerializadorFornecedor,
     SerializadorErro,
-    formatosAceitos: ['application/json'],
+    formatosAceitos: [
+        'application/json',
+        'application/xml',
+    ],
 }
