128cbd33d60b846b45b768d77602f32b1dada616
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-21 18:21:25 -0300

    GET by ID, POST and DELETE Routes

--- src/rotas/fornecedores/produtos/index.js
+++ src/rotas/fornecedores/produtos/index.js
@@ -1,15 +1,21 @@
 // Roteador Express
-const router = require('express').Router({ mergeParams: true })
+const roteador = require('express').Router()
 
-// DAO Tabela Produtos
+// DAO Tabela Produto
 const TabelaProduto = require('./TabelaProduto')
+
+// Model Produto
+const Produto = require('./Produto')
 
 // Serializador de respostas
 const Serializador = require('../../../Serializador').SerializadorProduto
 
-router.get('/', async (req, res) => {
-    const idFornecedor = req.params.idFornecedor
-    console.log(req.params);
+/**
+ * Setando As Rotas
+ */
+
+roteador.get('/', async (req, res) => {
+    const idFornecedor = req.fornecedor.id
 
     const produtos = await TabelaProduto.listar(idFornecedor)
 
@@ -19,4 +25,58 @@ router.get('/', async (req, res) => {
 
 })
 
-module.exports = router
+roteador.get('/:idProduto', async (req, res, next) => {
+    try {
+        const idFornecedor = req.fornecedor.id
+        const idProduto = req.params.idProduto
+
+        const produto = new Produto({ id: idProduto, fornecedor: idFornecedor })
+        await produto.carregar()
+
+        const serializador = new Serializador(res.getHeader('Content-Type'), ['estoque', 'dataCriacao', 'dataAtualizacao', 'versao',])
+
+        console.log(produto);
+
+        res.send(serializador.serializar(produto))
+    } catch (error) {
+        next(error)
+    }
+})
+
+roteador.post('/', async (req, res, next) => {
+
+    try {
+        const idFornecedor = req.fornecedor.id
+        const corpo = req.body
+
+        const dados = Object.assign({}, corpo, { fornecedor: parseInt(idFornecedor) })
+
+        const produto = new Produto(dados)
+        await produto.criar()
+
+        const serializador = new Serializador(res.getHeader('Content-type'), ['estoque', 'dataCriacao', 'dataAtualizacao', 'versao'])
+
+        res.status(201)
+        res.send(serializador.serializar(produto))
+    } catch (error) {
+        next(error)
+    }
+
+})
+
+roteador.delete('/:idProduto', async (req, res, next) => {
+    try {
+        const idFornecedor = req.fornecedor.id
+        const idProduto = req.params.idProduto
+
+        const produto = new Produto({ id: idProduto, fornecedor: idFornecedor })
+        await produto.apagar()
+
+        res.status(204)
+        res.end()
+    } catch (error) {
+        next(error)
+    }
+})
+
+module.exports = roteador
