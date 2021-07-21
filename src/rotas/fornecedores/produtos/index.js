10e405e28626abc5021c03874b0ca7f300b9ca3d
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-21 12:30:25 -0300

    Access to DAO and response Serializer

--- src/rotas/fornecedores/produtos/index.js
+++ src/rotas/fornecedores/produtos/index.js
@@ -1,7 +1,22 @@
-const router = require('express').Router()
+// Roteador Express
+const router = require('express').Router({ mergeParams: true })
 
-router.get('/', (req, res) => {
-    res.json([])
+// DAO Tabela Produtos
+const TabelaProduto = require('./TabelaProduto')
+
+// Serializador de respostas
+const Serializador = require('../../../Serializador').SerializadorProduto
+
+router.get('/', async (req, res) => {
+    const idFornecedor = req.params.idFornecedor
+    console.log(req.params);
+
+    const produtos = await TabelaProduto.listar(idFornecedor)
+
+    const serializador = new Serializador(res.getHeader('Content-Type'))
+
+    res.send(serializador.serializar(produtos))
+
 })
 
 module.exports = router
