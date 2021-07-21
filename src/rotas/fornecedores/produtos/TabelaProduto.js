d137db71d7ab1cdc41016310cd78b22ef01cff28
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-21 18:22:32 -0300

    Getbyid, insert, delete Func, and getList Bug Fix

--- src/rotas/fornecedores/produtos/TabelaProduto.js
+++ src/rotas/fornecedores/produtos/TabelaProduto.js
@@ -5,6 +5,29 @@ class TabelaProdutos {
         return Modelo.findAll({
             where: {
                 fornecedor: idFornecedor
+            },
+            raw: true,
+        })
+    }
+
+    pegarPorId(idFornecedor, idProduto) {
+        return Modelo.findOne({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor,
+            }
+        })
+    }
+
+    inserir(produto) {
+        return Modelo.create(produto)
+    }
+
+    apagar(idFornecedor, idProduto) {
+        return Modelo.destroy({
+            where: {
+                id: idProduto,
+                fornecedor: idFornecedor
             }
         })
     }
