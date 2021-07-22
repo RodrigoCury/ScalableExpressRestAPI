5c211c90eb418f45667904c42b3af0a3d21dbc3b
Rodrigo Cury <rodrigocury@ufu.br>
2021-07-22 16:33:23 -0300

    Better Error Message

--- src/erros/ProdutoNaoEncontrado.js
+++ src/erros/ProdutoNaoEncontrado.js
@@ -1,6 +1,6 @@
 class ProdutoNaoEncontrado extends Error {
     constructor() {
-        super("Produto não encontrado")
+        super("Produto não encontrado ou não pertence ao fornecedor solicitado")
         this.name = 'ProdutoNaoEncontrado'
         this.idErro = 4
         this.status = 404
